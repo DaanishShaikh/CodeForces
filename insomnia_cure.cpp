b9901362d414fa523b98783634dfe8c224a191bd
DaanishShaikh <daanish222@gmail.com>
2024-01-29 21:23:48 +0530

    update

--- insomnia_cure.cpp
+++ insomnia_cure.cpp
@@ -7,16 +7,16 @@ int main(){
     cin>>k>>l>>m>>n;
     int total;
     cin>>total;
-    if(total==1){
-        cout<<"1";
-    }
+    
     for(int i=0; i<total; i++){
         a[i]=i+1;
     }
     for(int j=0; j<total ; j++){
-        if(a[j]%k==0||a[j]%l==0||a[j]%m==0||a[j]%n==0){
+        
+            if(a[j]%k==0||a[j]%l==0||a[j]%m==0||a[j]%n==0){
             count++;
         }
+        
     }    
    cout<<count;
     return 0;
