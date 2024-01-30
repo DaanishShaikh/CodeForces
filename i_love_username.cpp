8c609ac2f1a9b14242d6d711a9680bbff0b9b0a7
DaanishShaikh <daanish222@gmail.com>
2024-01-30 11:02:18 +0530

    update

---
+++ i_love_username.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int a[100];
+int first=a[0];
+int morecount=0,lesscount=0;
+for(int i=0; i<n ; i++){
+    cin>>a[i];
+    cout<<a[i]<<endl;
+}
+for(int j=1 ; j<=n ; j++){
+    if(a[j]>a[0])
+}
+}
