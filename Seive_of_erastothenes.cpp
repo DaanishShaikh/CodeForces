291b11891e4776075160cb8eec7b2091d0107d10
DaanishShaikh <daanish222@gmail.com>
2024-01-31 20:26:58 +0530

    updates

---
+++ Seive_of_erastothenes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<=n ; i++){
+        a[i]=true;
+    }
+    for(int j=2 ; j<n ; j++){
+        if(a[j]==true){
+            for(int k=2*j ; k<=n ; k=k+j){
+                a[k]=false;
+            }
+        }
+    }
+    for (int z=0 ; z<=n ; z++){
+        if(a[z]==true){
+            cout<<z<<endl;
+        }
+    }
+    return 0;
+}
