f176a684e840280391365aed54fc2629b31343e8
DaanishShaikh <daanish222@gmail.com>
2024-02-01 16:32:30 +0530

    update

--- Arrival_of_the_colonel.cpp
+++ Arrival_of_the_colonel.cpp
@@ -6,29 +6,39 @@ int main(){
     int x,y;
     int arr[100];
     int min=arr[0];
-    int max=arr[0];
+    int max;
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
+    max=arr[0];
     for(int j=0 ; j<n ; j++){
-        if(arr[j]>max){
+        if(arr[j]>=max){
             max=arr[j];
-            x=j;
+            
+           
         }
           if(arr[j]<=min){
             min=arr[j];
-            y=j;
-        }
+            }
+        }    
+        for(int k=0; k<n ; k++){
+                if(arr[k]==max){
+                    x=k;
+                    break;
+                }
+                }
+        for(int z=n-1 ; z>=0 ; z--){
+                if(arr[z]==min){
+                        y=z;
+                        break;
+                    }
+                }
         
-    }    
     
     y=(n-1)-y;
-    
-    cout<<max<<endl;
-    cout<<min<<endl;
     cout<<x<<endl;
     cout<<y<<endl;
-    cout<<x+y;
+cout<<x+y;
     return 0;
     
 }
