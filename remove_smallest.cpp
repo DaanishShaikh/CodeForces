b07309a8abe061e1bd7421307ef31f634ac647bd
DaanishShaikh <daanish222@gmail.com>
2024-02-09 21:36:29 +0530

    09-02-2024

--- remove_smallest.cpp
+++ remove_smallest.cpp
@@ -5,27 +5,55 @@ int main(){
     int t;
     cin>>t;
     for(int i=0 ; i<t ; i++){
-        int l;
+        long long l;
         cin>>l;
-        vector<int>a(l);
+        vector<long long>a(l);
         for(int j=0 ; j<l ; j++){
             cin>>a[j];
         }
-        for(int k=0 ; k<l ; k++){
-            for(int m=0 ; m<l ; m++){
-                if(a.size()==1){
-                    break;
+        for(long long p=0 ; p<l ; p++){
+            for(long long q=0 ; q<l-1 ; q++){
+                
+                if(a[q]>a[q+1]){
+                    swap(a[q],a[q+1]);
                 }
-                if(a[k]-a[m]>1){
-                   cout<<"NO";
-            }
-            else{
-                cout<<"YES";
             }
         }
         for(int j=0 ; j<l ; j++){
             cout<<a[j];
         }
+        bool flag=false;
+        // for(int k=0 ; k<l ; k++){
+        //     for(int m=0 ; m<l ; m++){
+        //         if(a[k]-a[m]>1||a[k]-a[m]<-1){
+                    // cout<<"a[k]="<<a[k]<<"a[m]="<<a[m]<<"dif="<<a[k]-a[m]<<endl<<m;
+        //            flag=true;
+        //            break;
+        //            }            
+        // }
+         for(long long q=0 ; q<l-1 ; q++){
+                
+                if(a[q]-a[q+1]>1||a[q]-a[q+1]<-1){
+                    flag = true;
+                                        cout<<"a[k]="<<a[q]<<"a[m]="<<a[q+1]<<"dif="<<a[q]-a[q+1]<<endl<<q;
+
+                }
+                 cout<<"a[k]="<<a[q]<<"a[m]="<<a[q+1]<<"dif="<<a[q]-a[q+1]<<endl<<q;
+                if(flag){break;}
+            }
+        
+        
+        if(flag){
+            cout<<"NO"<<endl;
+            
+        }
+        if(flag==false){
+            cout<<"YES"<<endl;
+                                                   
+
+        }
+
     }
-    return 0;
-}
+                return 0;
+
+    }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,1 @@
-        cout<<sumf<<' ';
+                    // cout<<"a[k]="<<a[k]<<"a[m]="<<a[m]<<"dif="<<a[k]-a[m]<<endl<<m;
