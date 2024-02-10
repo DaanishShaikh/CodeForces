5a4104d9ad9fdc3d2cf4231b2564ae6117db69cd
DaanishShaikh <daanish222@gmail.com>
2024-02-10 15:12:47 +0530

    10-02-2024

--- good_pair.cpp
+++ good_pair.cpp
@@ -3,60 +3,36 @@
 #include<cmath>
 using namespace std;
 int main(){
-    long long t;
+    int t;
     cin>>t;
 
-    for(long long i=0 ; i<t ; i++){
-        long long l;
+    for(int i=0 ; i<t ; i++){
+        int l;
         cin>>l;
-        vector <long long>a(l);
-        for(long long j=0 ; j<l ; j++){
+        vector <int>a(l);
+        for(int j=0 ; j<l ; j++){
             cin>>a[j];
         }
-        long long max=a[0];
-        long long min=a[0];
+        if(l==1){
+        cout<<"1"<<" "<<"1"<<endl;
+    }
+        if(l>1){
+        int max=a[0];
+        int min=a[0];
+        int max_ind=0, min_ind=0;
         for(int k=0 ; k<l ; k++){
             if(a[k]>max){
                 max=a[k];
+                max_ind=k;
             }
             if(a[k]<min){
                 min=a[k];
+                min_ind=k;
             }
         }
-        cout<<"max="<<max<<"min="<<min;
-        
-    //     if(l==1){
-    //         cout<<"1"<<" "<<"1"<<endl;
-            
-    //     }
-    //     if(l>1){
-    //     for(long long n=0 ; n<l ; n++){
-    //         for(long long k=0 ; k<l-n-1 ; k++){
-    //             if(a[k]>a[k+1]){
-    //              swap(a[k],a[k+1]);
-    //         }
-    //     }
-    //     }
-    //     long long first=a[0];
-    //     long long last=a[l-1];
-        
-    //     bool flag=false;
-    //     long long ind1,ind2;
-    //     for(long long p=0 ; p<l ; p++){
-            
-    //             if(first==aog[p]){
-    //                 ind1=p+1;
-    //                 break;
-    //             }
-    //     }
-    //     for(long long p=0 ; p<l ; p++){
-    //             if(last==aog[p]){
-    //                 ind2=p+1;
-    //                 break;
-    //             }
-    //     }
-    //         cout<<ind1<<" "<<ind2<<endl;
-    //     }   
-    // }
+        cout<<min_ind+1<<" "<<max_ind+1<<endl;
+    }    
+    }
+    
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -10,47 +10,29 @@ int main(){
         long long l;
         cin>>l;
         vector <long long>a(l);
-        vector<long long>aog(l);
         for(long long j=0 ; j<l ; j++){
             cin>>a[j];
-            aog[j]=a[j];
-        }
-        for(long long n=0 ; n<l ; n++){
-            for(long long k=0 ; k<l-1 ; k++){
-                if(a[k]>a[k+1]){
-                 swap(a[k],a[k+1]);
-                //  cout<<a[k]<<" "<<a[k+1];
-            }
-        }
-        }
-        // for(int m=0 ; m<l ; m++){
-        //     cout<<a[m];
-        // }
-        long long first=a[0];
-        long long last=a[l-1];
-        bool flag=false;
-        // for(long long o=0 ; o<l ; o++){
-        //     if(abs(last-first)==a[o]){
-        //         flag=true;
-        //     }
-        // }
-        long long ind1,ind2;
-        for(long long p=0 ; p<l ; p++){
-            
-                if(first==aog[p]){
-                    ind1=p+1;
-                }
-                if(last==aog[p]){
-                    ind2=p+1;
-                }
-            
-        }
-        if(l>1){
-            cout<<ind1<<" "<<ind2<<endl;
         }
         if(l==1){
-            cout<<"1"<<" "<<"1";
-        }
+        cout<<"1"<<" "<<"1";
     }
+        if(l>1){
+        long long max=a[0];
+        long long min=a[0];
+        long long max_ind=0, min_ind=0;
+        for(long long k=0 ; k<l ; k++){
+            if(a[k]>max){
+                max=a[k];
+                max_ind+=k+1;
+            }
+            if(a[k]<min){
+                min=a[k];
+                min_ind+=k+1;
+            }
+        }
+        cout<<min_ind<<" "<<max_ind<<endl;
+    }    
+    }
+    
     return 0;
 }
