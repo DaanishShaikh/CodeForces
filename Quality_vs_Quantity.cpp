b47a681c2e68f4e96fca6df3580a246e0a0e505a
DaanishShaikh <daanish222@gmail.com>
2024-02-11 19:01:50 +0530

    update

--- Quality_vs_Quantity.cpp
+++ Quality_vs_Quantity.cpp
@@ -43,7 +43,7 @@ int main(){
         sumb=sumb+a[b];
         // cout<<"start"<<a[k]<<" ";
         // cout<<"end"<<a[b]<<" ";
-        cout<<"sumf"<<sumf<<' ';
+        // cout<<"sumf"<<sumf<<' ';
         cout<<"sumd"<<sumb<<' ';
 
         if(sumb>sumf){
