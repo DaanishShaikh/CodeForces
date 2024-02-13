2caa88992e8a0442b0a0f6269cd6eb6e28646417
DaanishShaikh <daanish222@gmail.com>
2024-02-13 19:56:42 +0530

    13-02-2024

--- Word.cpp
+++ Word.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
 #include<cctype>
+#include<string>
 using namespace std;
 int main(){
     string s;
     cin>>s;
-    for(int i=0 ; i<size(s) ; i++){
-        if()
+    int countu=0, countl=0;
+    for(int i=0 ; i<s.size() ; i++){
+        if(isupper(s[i])){
+            countu++;
+        }
+        if(islower(s[i])){
+            countl++;
+        }
     }
-    
+    if(countu>countl){
+        for(int i=0; i<s.size() ; i++){
+        s[i]=toupper(s[i]);
+        }
+    }
+    else if(countu<countl){
+        for(int i=0; i<s.size() ; i++){
+        s[i]=tolower(s[i]);
+        }
+    }
+    else{
+        for(int i=0; i<s.size() ; i++){
+        s[i]=tolower(s[i]);
+        }
+    }
+    return 0;
     }
