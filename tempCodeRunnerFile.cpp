39af2924883163d8f44ced5e644405115a050834
DaanishShaikh <daanish222@gmail.com>
2024-02-13 23:41:17 +0530

    13-02-2024

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -9,4 +9,12 @@ for (int i = 0; i < t; i++) {
         int n;
         cin >> n;
         
-        
+        if (n <= 26) {
+            if (n == 3) {
+                cout << "aaa" << endl;
+            } else {
+                int x = n - 2 - 1 + 97;
+                char z = static_cast<char>(x);
+                cout << "aa" << z << endl;
+            }
+        } 
