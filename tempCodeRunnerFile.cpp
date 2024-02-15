53f9d0620e32516b5de6e1db9d8621883f6dbf48
DaanishShaikh <daanish222@gmail.com>
2024-02-15 12:25:00 +0530

    15-02-2024

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,36 +1,13 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    int t;
-    cin >> t;
-    
-for (int i = 0; i < t; i++) {
-        int n;
-        cin >> n;
-        
-        if (n <= 26) {
-            if (n == 3) {
-                cout << "aaa" << endl;
-            } else {
-                int x = n - 2 - 1 + 97;
-                char z = static_cast<char>(x);
-                cout << "aa" << z << endl;
-            }
-        } else if (n > 52) {
-            int x = n + 97 - 52 - 1;
-            char z = static_cast<char>(x);
-            cout << z << "zz" << endl;
-        }
-        else if (n == 27) {
-            cout << "aay" << endl;
-        } else {
-            int x = n - 26 + 96 - 1;
-            char z = static_cast<char>(x);
-            cout << "a" << z << "z" << endl;
-        }
+int main(){
+    int n,m;
+    cin>>n>m;
+    if(n<m){
+        cout<<"YES";
     }
-    
-    return 0; 
+    else{
+        cout<<"NO";
+    }
+    return 0;
 }
-    
