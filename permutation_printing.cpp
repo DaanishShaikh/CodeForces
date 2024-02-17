1612f8b6dc0f4c56f188f056dd131267f0725fce
DaanishShaikh <daanish222@gmail.com>
2024-02-17 22:34:18 +0530

    17-02-2024

--- permutation_printing.cpp
+++ permutation_printing.cpp
@@ -13,7 +13,7 @@ int main(){
         }
         // cout<<endl;
         // for(int j=n-1 ; j>0 ; j--){
-        //         swap(a[j],a[j-1]);
+                swap(a[j],a[j-1]);
                 }
         for(int j=0 ; j<n ; j++){
             cout<<a[j]<<endl;
