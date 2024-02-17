ee617f66abe7272f89b1cf8db7139fe6bea01296
DaanishShaikh <daanish222@gmail.com>
2024-02-17 21:21:39 +0530

    17-02-2024

---
+++ maximise_the_score.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0; i<t ; i++){
+        int n;
+        cin>>n;
+        vector<int> a(2*n);
+        for(int j=0 ; j<2*n ; j++){
+            cin>>a[j];
+        }
+        int score=0;
+        int mini;
+        sort(a.begin(), a.end());
+        // for(int j=0 ; j<2*n ; j++){
+        //     cout<<a[j]<<' ';
+        // }
+        for(int j=0 ; j<2*n ; j+=2){
+            score+=a[j];
+        }
+        cout<<score<<endl;
+    }
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,16 +1,1 @@
-hi
-hello
-hello hi
-hello world
-hhi world
-hello hi world
-yo man
-how u doing
-doing well??
-i hope so
-right??
-anyways
-got the black dot yet??
-not yet??
-oh need to push 20 times right??
-a little too much thanks to your mistake!
+        // cout<<score<<endl;
