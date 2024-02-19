e1672ca3b56dc7b74815e21dd9aec047ba1e9ff4
DaanishShaikh <daanish222@gmail.com>
2024-02-19 12:11:16 +0530

    19-02-2024

--- LR_remainder.cpp
+++ LR_remainder.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-// int main(){
-//     long long t;
-//     cin>>t;
+int main(){
+    long long t;
+    cin>>t;
 //     for(long long i=0 ; i<t ; i++){
 //         long long n;
 //         cin>>n;
