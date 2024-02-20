0f037de3bed9ba65f054b6523f7681228becf933
DaanishShaikh <daanish222@gmail.com>
2024-02-20 09:32:35 +0530

    20-02-2024

--- vlad_and_a_sum_of_sum_of_digits.cpp
+++ vlad_and_a_sum_of_sum_of_digits.cpp
@@ -10,13 +10,19 @@ vector<int>a(n);
         for(int j=0; j<n; j++){
             a[j]=j+1;
         }
+        int multiple(n/9);
+        int ten=10;
+        for(int j=0 ; j<n ; j++){
+            multiple[j]=11+j;
+        }
+        int count=1;
         for(int j=0 ; j<n ; j++){
             if(a[j]<=9){
                 a[j]=j+1;
             }
-            else{
-                string s=a[j];
-                
+            else if(a[j]==(9*count)+1){
+                for
+                count++;
             }
         }
     }
