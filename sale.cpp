b81f04ff3f64e33c037727177a9994d41acc18fc
DaanishShaikh <daanish222@gmail.com>
2024-02-21 16:07:37 +0530

    21-02-2024

--- sale.cpp
+++ sale.cpp
@@ -1,4 +1,7 @@
 // worked and accepted
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4.1,sse4.2,sse4a,avx,avx2,popcnt,tune=native")
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
