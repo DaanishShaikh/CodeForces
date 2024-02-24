57cff3d4181d523756bf91791b8684c9a77aef82
DaanishShaikh <daanish222@gmail.com>
2024-02-24 22:52:09 +0530

    24-02-2024

---
+++ Team.cpp
@@ -0,0 +1,78 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int tt;
+//     cin>>tt;
+//     while(tt--){
+//         int n;
+//         cin>>n;
+//         int anscount=0;
+         vector<vector<int>>v(n,vector<int>(3));
+//         int count=0;
+//         for(int j=0; j<n ; j++){
+//         for(int i=0 ; i<3 ; i++){
+//             cin>>v[j][i];
+//             cout<<v[j][i]<<endl;
+//         }
+//         cout<<endl;
+//         }
+//         for(int j=0; j<n ; j++){
+//         for(int i=0 ; i<3 ; i++){
+//             cout<<v[j][i];
+//         }
+//         cout<<endl;
+//         }
+
+//         for(int j=0 ; j<n ; j++){
+//         for(int k=0 ; k<3 ; k++){
+//             if(v[j][k]==1){
+//                 count++;
+//             }
+//         }
+//         if(count>=2){anscount++;}
+//         }
+        
+//         cout<<anscount<<endl;
+        
+//     }
+//     return 0;
+// }
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tt;
+    cin >> tt;
+    while(tt--) {
+        int n;
+        cin >> n;
+        int anscount = 0;
+        vector<vector<int>> v(n, vector<int>(3)); // Correct way to initialize a 2D vector
+        int count = 0;
+        for(int j = 0; j < n; j++) {
+            for(int i = 0; i < 3; i++) {
+                cin >> v[j][i];
+            }
+        }
+        for(int j = 0; j < n; j++) {
+            for(int i = 0; i < 3; i++) {
+                cout << v[j][i] << " ";
+            }
+            cout << endl;
+        }
+        for(int j = 0; j < n; j++) {
+            count = 0; // Reset count for each row
+            for(int k = 0; k < 3; k++) {
+                if(v[j][k] == 1) {
+                    count++;
+                }
+            }
+            if(count >= 2) {
+                anscount++;
+            }
+        }
+        
+        cout << anscount << endl;
+    }
+    return 0;
+}
--- chaya_calendar.cpp
+++ chaya_calendar.cpp
@@ -1,36 +1,66 @@
+/*code did not work hence commented*/
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int t;
+//     cin>>t;
+//     for(int i=0 ; i<t ; i++){
+//         int n;
+//         cin>>n;
+//         int a[n];
+//         for(int j=0 ; j<n ; j++){
+//             cin>>a[j];
+//         }
+//         int count=0;
+//         int temp=0;
+//         for(int j=0 ; j<n ; j++){
+//             temp=a[j];
+//             // cout<<"temp1="<<temp<<endl;
+//             if(temp>count){
+//                 count=temp;
+//                 // cout<<"count1="<<count<<' ';
+//             }
+            
+//             else{
+//                 while(temp<=count){
+//                     temp=temp+a[j];
+//                 }
+//                 // cout<<"temp2="<<temp<<' ';
+//                 count=temp;
+//                 // cout<<"count2="<<count<<' ';
+//             }
+//         }
+//         cout<<count<<endl;
+//     }
+//     return 0;
+
+// }
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int t;
-    cin>>t;
-    for(int i=0 ; i<t ; i++){
+    int tt;
+    cin>>tt;
+    while(tt--){
         int n;
         cin>>n;
-        int a[n];
-        for(int j=0 ; j<n ; j++){
-            cin>>a[j];
+        vector<int>v(n);
+        for(int i=0 ; i<n ; i++){
+            cin>>v[i];
         }
-        int count=0;
-        int temp=0;
-        for(int j=0 ; j<n ; j++){
-            temp=a[j];
-            // cout<<"temp1="<<temp<<endl;
-            if(temp>count){
-                count=temp;
-                // cout<<"count1="<<count<<' ';
+        // for(int i=0 ; i<n ; i++){
+        //     cout<<v[i];
+        // }
+        //cout<<endl;
+        int max=v[0],multiple=0;
+        for(int i=1 ; i<n ; i++){
+            if(v[i]<max){multiple=max/v[i]+1;max=v[i]*multiple;//cout<<"max="<<max<<' ';
             }
-            
-            else{
-                while(temp<=count){
-                    temp=temp+a[j];
-                }
-                // cout<<"temp2="<<temp<<' ';
-                count=temp;
-                // cout<<"count2="<<count<<' ';
+            if(v[i]==max){max+=v[i];//cout<<"max="<<max<<' ';
+            }
+            if(v[i]>max){max=v[i];//cout<<"max="<<max<<' ';
             }
         }
-        cout<<count<<endl;
+        cout<<max<<endl;
     }
     return 0;
-
 }
