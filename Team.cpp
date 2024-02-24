93dba2934da6eafa4eb2b24f50c8d5e1340a67fc
DaanishShaikh <daanish222@gmail.com>
2024-02-24 22:53:53 +0530

    24-02-2024

--- Team.cpp
+++ Team.cpp
@@ -12,7 +12,7 @@
         for(int j=0; j<n ; j++){
         for(int i=0 ; i<3 ; i++){
             cin>>v[j][i];
-//             cout<<v[j][i]<<endl;
+            cout<<v[j][i]<<endl;
 //         }
 //         cout<<endl;
 //         }
