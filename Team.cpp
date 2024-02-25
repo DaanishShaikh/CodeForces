2477d8d45559b59be01388ba568e47e9c3ef328e
DaanishShaikh <daanish222@gmail.com>
2024-02-25 21:43:38 +0530

    25-02-2024

--- Team.cpp
+++ Team.cpp
@@ -1,78 +1,19 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     int tt;
-//     cin>>tt;
-//     while(tt--){
-//         int n;
-//         cin>>n;
-//         int anscount=0;
-         vector<vector<int>>v(n,vector<int>(3));
-        int count=0;
-        for(int j=0; j<n ; j++){
-        for(int i=0 ; i<3 ; i++){
-            cin>>v[j][i];
-            cout<<v[j][i]<<endl;
-        }
-        cout<<endl;
-        }
-        for(int j=0; j<n ; j++){
-        for(int i=0 ; i<3 ; i++){
-            cout<<v[j][i];
-        }
-        cout<<endl;
-//         }
-
-//         for(int j=0 ; j<n ; j++){
-//         for(int k=0 ; k<3 ; k++){
-//             if(v[j][k]==1){
-//                 count++;
-//             }
-//         }
-//         if(count>=2){anscount++;}
-//         }
-        
-//         cout<<anscount<<endl;
-        
-//     }
-//     return 0;
-// }
 #include<bits/stdc++.h>
 using namespace std;
-
-int main() {
+ int main(){
     int tt;
-    cin >> tt;
-    while(tt--) {
+    cin>>tt;
+    while(tt--){
         int n;
-        cin >> n;
-        int anscount = 0;
-        vector<vector<int>> v(n, vector<int>(3)); // Correct way to initialize a 2D vector
-        int count = 0;
-        for(int j = 0; j < n; j++) {
-            for(int i = 0; i < 3; i++) {
-                cin >> v[j][i];
-            }
+        cin>>n;
+        int a[n*3];
+        for(int i=0 ; i<n*3 ; i++){
+            cin>>a[i];
         }
-        for(int j = 0; j < n; j++) {
-            for(int i = 0; i < 3; i++) {
-                cout << v[j][i] << " ";
-            }
-            cout << endl;
+        cout<<endl;
+        for(int i=0 ; i<n*3 ; i++){
+            cout<<a[i];
         }
-        for(int j = 0; j < n; j++) {
-            count = 0; // Reset count for each row
-            for(int k = 0; k < 3; k++) {
-                if(v[j][k] == 1) {
-                    count++;
-                }
-            }
-            if(count >= 2) {
-                anscount++;
-            }
-        }
-        
-        cout << anscount << endl;
     }
     return 0;
-}
+ }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,4 @@
-        for(int i=0 ; i<3 ; i++){
+for(int j = 0; j < n; j++) {
+        //     for(int i = 0; i < 3; i++) {
+        //         cout << v[j][i] << " ";
+        //     }
