7a4ead079a60bb5ea45831839d4668e40d028ec0
DaanishShaikh <daanish222@gmail.com>
2024-02-26 23:06:45 +0530

    26-02-2024

--- theatre_square.cpp
+++ theatre_square.cpp
@@ -1,9 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+using i64=long long;
 int main(){
-    int n, m, a;
+    i64 n, m, a;
     cin>>n>>m>>a;
-    int count=0;
-
+    i64 count=0,length=0,breadth=0;
+    if(n%a==0){
+        length=n/a;
+    }
+    else{
+        length=(n/a)+1;
+    }
+    if(m%a==0){
+        breadth=m/a;
+    }
+    else{
+        breadth=(m/a)+1;
+    }
+    i64 area=length*breadth;
+    cout<<area<<endl;
     return 0;
 } 
