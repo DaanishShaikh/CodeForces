d9e56e851e35558321ee0d9a091c84539c55657a
DaanishShaikh <daanish222@gmail.com>
2024-02-26 13:58:53 +0530

    26-02-2024

--- theatre_square.cpp
+++ theatre_square.cpp
@@ -4,57 +4,6 @@ int main(){
     int n, m, a;
     cin>>n>>m>>a;
     int count=0;
-    if((n<a&&m<a)||(n==a&&m==a)){
-        cout<<'1';
-    }
-    if(a==1){
-        cout<<n*m;
-    }
-    if(n>a&&m<=a){
-        cout<<n;
-    }
-    if(m>a&&n<=a){
-        cout<<m;
-    }
-    else{
-        if(n%a>=1){
-            count=(n/a)+1;
-            // cout<<'1';
-        }
-        if(n%a==0){
-            count=n/a;
-            // cout<<'2';
-        }
-        if(m%a==0){
-            count+=(m/a)-1;
-            // cout<<'3';
-        }
-        if(m%a>=1){
-            count+=m/a;
-            // cout<<'4';
-        }
-        int odd=m/a;
-        count+=2*odd-1;
-        cout<<count<<endl;
-    }
-    
-    
-    // else if(n>a&&m<=a){
-    //     if(n%a==0){
-    //         cout<<n/a;
-    //     }
-    //     else{cout<<(n/a)+1;}
-    // }
-    // else if(m>a&&n<=a){
-    //     if(m%a==0){
-    //         cout<<m/a;
-    //     }
-    //     else{cout<<(m/a)+1;}
-    // }
-    // else if(m>a&&n>a){
-    //     if(m%)
-    // }
-    
 
     return 0;
 } 
