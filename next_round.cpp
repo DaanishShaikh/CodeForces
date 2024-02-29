d3bd471240b80f5e23595948ea51df7a484b1488
DaanishShaikh <daanish222@gmail.com>
2024-02-29 20:19:05 +0530

    29-02-2024

--- next_round.cpp
+++ next_round.cpp
@@ -7,8 +7,21 @@ int main(){
     for(int i=0 ; i<n ; i++){
         cin>>v[i];
     }
+    int flag=false;
     for(int i=0 ; i<n ; i++){
-        cout<<v[i];
+        if(v[i]<=0){flag=true;}
+        if(v[i]>0){flag=false;}
     }
-    
+    if(flag){cout<<'0';}
+    else{
+        int kv=v[k-1];
+        int count=0;
+        sort(v.begin(),v.end());
+        for(int i=0 ; i<n ; i++){
+            if(v[i]>=kv){count++;}
+        }
+    }
+    cout<<count<<endl;
+
+    return 0;
 }
