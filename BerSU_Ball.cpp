2b43a260c61f2d6199f0a0effb9da17bf6bd90af
DaanishShaikh <daanish222@gmail.com>
2024-03-01 18:09:49 +0530

    01-03-2024

---
+++ BerSU_Ball.cpp
@@ -0,0 +1,38 @@
+/*Accepted*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    int m;
+    cin >> m;
+    vector<int> g(m);
+    for (int i = 0; i < m; i++) {
+        cin >> g[i];
+    }
+
+    sort(b.begin(), b.end());
+    sort(g.begin(), g.end());
+
+    set<int> used;
+
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (abs(b[i] - g[j]) <= 1 && used.find(j) == used.end()) {
+                count++;
+                used.insert(j);
+                break;
+            }
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,32 +1,5 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n,k;
-    cin>>n>>k;
-    int count=0;
-    vector<int>v(n);
-    for(int i=0 ; i<n ; i++){
-        cin>>v[i];
-    }
-    int flag=false;
-    int countzero=0;
-    for(int i=0 ; i<n ; i++){
-        if(v[i]==0){countzero++;}
-    }
-    if(countzero==n){cout<<'0';}
-    else{
-        int kv=v[k-1];
-        sort(v.begin(),v.end());
-        for(int i=0 ; i<n ; i++){
-            if(v[i]==0){continue;}
-            if(v[i]>=kv){
-                
-                count++;
+                cout<<b[i]<<' '<<g[j]<<endl;
+                for(int i=0 ; i<g.size() ; i++){
+                    cout<<g[i]<<' ';
                 }
-        }
-        cout<<count<<endl;
-    }
-    
-
-    return 0;
-}
+                cout<<endl;
