d5668213048d12d3c57f4412a04c2921f7f3b825
DaanishShaikh <daanish222@gmail.com>
2024-03-01 16:47:26 +0530

    01-03-2024

---
+++ bit++.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+    int n;
+    cin>>n;
+    string ss;
+    vector<string>s[n];
+    for(int i=0 ; i<n ; i++){
+        cin>>ss;
+        s[i].push_back(ss);
+    }
+    for(int i=0 ; i<n ; i++){
+        
+    }
+    return 0;
+ }
