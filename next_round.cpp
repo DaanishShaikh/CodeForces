02c32c4e6ad21e6bcb9beb7f941f690c3d71d7ce
DaanishShaikh <daanish222@gmail.com>
2024-03-01 10:39:03 +0530

    01-03-2024

--- next_round.cpp
+++ next_round.cpp
@@ -3,25 +3,30 @@ using namespace std;
 int main(){
     int n,k;
     cin>>n>>k;
+    int count=0;
     vector<int>v(n);
     for(int i=0 ; i<n ; i++){
         cin>>v[i];
     }
     int flag=false;
+    int countzero=0;
     for(int i=0 ; i<n ; i++){
-        if(v[i]<=0){flag=true;}
-        if(v[i]>0){flag=false;}
+        if(v[i]==0){countzero++;}
     }
-    if(flag){cout<<'0';}
+    if(countzero==n){cout<<'0';}
     else{
         int kv=v[k-1];
-        int count=0;
         sort(v.begin(),v.end());
         for(int i=0 ; i<n ; i++){
-            if(v[i]>=kv){count++;}
+            if(v[i]==0){continue;}
+            if(v[i]>=kv){
+                
+                count++;
+                }
         }
+        cout<<count<<endl;
     }
-    cout<<count<<endl;
+    
 
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,32 @@
-for(int j = 0; j < n; j++) {
-        //     for(int i = 0; i < 3; i++) {
-        //         cout << v[j][i] << " ";
-        //     }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int count=0;
+    vector<int>v(n);
+    for(int i=0 ; i<n ; i++){
+        cin>>v[i];
+    }
+    int flag=false;
+    int countzero=0;
+    for(int i=0 ; i<n ; i++){
+        if(v[i]==0){countzero++;}
+    }
+    if(countzero==n){cout<<'0';}
+    else{
+        int kv=v[k-1];
+        sort(v.begin(),v.end());
+        for(int i=0 ; i<n ; i++){
+            if(v[i]==0){continue;}
+            if(v[i]>=kv){
+                
+                count++;
+                }
+        }
+        cout<<count<<endl;
+    }
+    
+
+    return 0;
+}
