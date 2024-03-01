2938b42260fef977087157bc04178f42cf655b1e
DaanishShaikh <daanish222@gmail.com>
2024-03-01 22:16:31 +0530

    01-03-2024

---
+++ yet_another_coin_problem.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+using i64=long long;
+int main(){
+    i64 tt;
+    cin>>tt;
+    while(tt--){
+        i64 n;
+        cin>>n;
+        vector<int>v={15,10,6,3,1};
+        i64 rem=1,count=0,q=n;
+        if(n==1){cout<<'1'<<endl;}
+        else{
+        for(int i=0 ; i<4 ; i++){
+            if(v[i]<q){
+            rem=q%v[i];
+            q=q/v[i];
+            count+=q;
+            cout<<"v["<<i<<"]="<<v[i]<<' '<<"rem="<<rem<<' '<<"q="<<q<<' '<<"count="<<count<<endl;
+            }
+            if(rem=1){break;}
+
+        }
+        cout<<count<<endl;
+        }
+    }
+    return 0 ;
+}
