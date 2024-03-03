554ebf387d6cdc7b8997a72f6dc6e7e06245fa65
DaanishShaikh <daanish222@gmail.com>
2024-03-03 22:08:34 +0530

    03-03-2024

--- distinct_strings.cpp
+++ distinct_strings.cpp
@@ -4,29 +4,25 @@ int main(){
     int tt;
     cin>>tt;
     while(tt--){
-        pair<char,int> latinpair[26]={{'a', 0}, {'b', 0}, {'c', 0}, {'d', 0}, {'e', 0}, {'f', 0},
-        {'g', 0}, {'h', 0}, {'i', 0}, {'j', 0}, {'k', 0}, {'l', 0},
-        {'m', 0}, {'n', 0}, {'o', 0}, {'p', 0}, {'q', 0}, {'r', 0},
-        {'s', 0}, {'t', 0}, {'u', 0}, {'v', 0}, {'w', 0}, {'x', 0},
-        {'y', 0}, {'z', 0}};
         int n;
         cin>>n;
         string s;
         cin>>s;
-        for(int i=0 ; i<s.size() ; i++ ){
-            for(int j=0 ; i<26;j++){
-                if(s[i]==latinpair[j].first){
-                    latinpair[j].second=1;
-                    break;
-                }
-            }
+        string temp, temps;
+        int i=0;
+        temps=s;
+        int count=0;
+        int j=n;
+        while(j--){
+        count++;
+        temp=s[i];     
+        temps.erase(temps.find(s[i]),1);
+        temps.push_back(temp[0]);
+        if(temps==s)break;
+        //cout<<"i="<<i<<"temp="<<temp<<' '<<"temps="<<temps<<endl;
+        i++;
         }
-        int sum=0;
-        for(int i=0; i<26 ; i++){
-            sum+=latinpair[i].second;
-            //cout<<"latinpair["<<i<<"].second="<<latinpair[i].second<<endl;
-        }
-        cout<<sum<<endl;
+        cout<<count<<endl;
     }
     return 0;
 }
