47261a9ceeb7464cf976c7b5f185572d83a8f25e
DaanishShaikh <daanish222@gmail.com>
2024-03-04 23:09:35 +0530

    04-03-2024

--- helpful_maths.cpp
+++ helpful_maths.cpp
@@ -7,10 +7,16 @@ int main(){
     for(int i=0 ; i<s.size() ; i+=2){
         string si(1,s[i]);
             v.push_back(si);
-            cout<<"i="<<i<<endl;
+            //cout<<"i="<<i<<endl;
     }
+    // for(int i=0 ; i<v.size(); i++){
+    //     cout<<v[i]<<' ';
+    // }
+    //cout<<endl;
+    sort(v.begin(),v.end());
     for(int i=0 ; i<v.size(); i++){
-        cout<<v[i]<<' ';
+        cout<<v[i];
+        if(i!=v.size()-1)cout<<'+';
     }
     return 0;
 }
