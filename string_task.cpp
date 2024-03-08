5b0addca010f3407b6681bd4cfd1a5b6f2159dff
DaanishShaikh <daanish222@gmail.com>
2024-03-08 12:28:15 +0530

    08-03-2024

--- string_task.cpp
+++ string_task.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main(){
     pair<pair<char,char>,int> latinpair[26]={
-        {{'a', 'A'}, 1}, {{'b', 'B'}, 2}, {{'c', 'C'}, 3}, {{'d', 'D'}, 4}, 
+        {{'a', 'A'}, 1}, {{'b', 'B'}, 2}, {{'c', 'C'}, 3}, {{'d', 'D'}, 4}, {{'e', 'E'}, 5},
+        {{'f', 'F'}, 6}, 
     };
     string s;
     cin>>s;
