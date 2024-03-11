cadd68fd3e9bc83c537580e4efb81895f51c3d4f
DaanishShaikh <daanish222@gmail.com>
2024-03-11 22:50:48 +0530

    11-03-2024

---
+++ rudolph_and_121.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool check(int n, vector<int>v){
+    for(int i=0 ; i<n ; i++){
+
+    }
+}
+
+
+
+
+
+int main(){
+    int tt;
+    cin>>tt;
+    while(tt--){
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0 ; i<n ; i++){
+            cin>>v[i];
+        }
+        for(int j=0 ; j<n ; j++){    
+            for(int i=1 ;i<n ; i++){
+                if()
+                v[i-1]=v[i-1]-1;
+                v[i]=v[i]-2;
+                v[i+1]=v[i+1]-1;
+            }
+        }    
+    }
+}
