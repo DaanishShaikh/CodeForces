7c1e7f7d7765505058df084ba9fc5b7c98cdece1
DaanishShaikh <daanish222@gmail.com>
2024-03-15 23:57:45 +0530

    15-03-2024

--- Array_fix.cpp
+++ Array_fix.cpp
@@ -3,4 +3,12 @@ using namespace std;
 int main(){
     int tt;
     cin>>tt;
-    
+    while(tt--){
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        vector<int>w;
+        for(int i=0 ; i<n ; i++){
+            cin>>v[i];
+        }
+        
