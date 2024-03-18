2ba3e34c40ec05607a25ec1857ae8807d5bc3c4c
DaanishShaikh <daanish222@gmail.com>
2024-03-18 23:37:07 +0530

    18-03-2024

--- cover_in_water.cpp
+++ cover_in_water.cpp
@@ -4,16 +4,12 @@ int main(){
     int tt;
     cin>>tt;
     while(tt--){
+        int n;
+        cin>>n;
         string s;
         cin>>s;
         bool flag=false;
         int count=0;
-        for(int i = 0 ; i<s.size()-2 ; i++){
-            if(s[i]=='.'&&s[i+1]=='.'&&s[i+2]=='.'){
-                flag=true;
-                break;
-            }
-        }
         for(int i = 0 ; i<s.size() ; i++){
             if(s[i]=='.'){
                 count++;
