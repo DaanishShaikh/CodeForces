5841b6b936a8d8f1ddfacc656f5d6d9b9c3148d9
DaanishShaikh <daanish222@gmail.com>
2024-03-19 18:33:39 +0530

    19-03-2024

---
+++ fireworks.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int tt;
+    cin >> tt;
+    while(tt--){
+        int a, b, m;
+        cin >> a >> b >> m;
+        if(a>m&&b>m){
+            if(abs(a-b)<=m){
+                cout<<'2'<<"\n";
+            }
+        }
+        if(a>m&&b<m){
+            
+        }
+    }
+}
