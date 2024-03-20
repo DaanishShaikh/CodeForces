a03a6f66c1afae6be5b2218ce596297ab5bb7038
DaanishShaikh <daanish222@gmail.com>
2024-03-21 00:57:56 +0530

    21-03-2024

--- doremys_paint_three.cpp
+++ doremys_paint_three.cpp
@@ -10,13 +10,6 @@ int main(){
         for(int i=0 ; i<n ; i++){
             cin>>v[i];
         }
-            //cout<<"maxcount="<<maxcount<<endl;
-            if(maxcount==n/2||v.size()-maxcount==n/2){
-                cout<<"Yes"<<endl;
-            } else {
-                cout<<"No"<<endl;
-            }
-        }
     }
     return 0;
 }
