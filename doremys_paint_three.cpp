3f7f2660b8b6a2adc1d112455a7bcfcdc631670a
DaanishShaikh <daanish222@gmail.com>
2024-03-20 18:23:41 +0530

    20-03-2024

--- doremys_paint_three.cpp
+++ doremys_paint_three.cpp
@@ -1,5 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+int check (vector<int>v){
+    sort(v.begin(),v.end());
+    int count=0;
+    for(int i=1 ; i<v.size(); i++){
+        if(v[i]!=v[i-1]){
+            count++;
+        }
+    }
+    return count;
+}
+bool check1 (vector<int>v){
+    for(int i=0 ; i<v.size() ; i++){
+        if(v[i]!=v[0]){
+            return false;
+        }
+    }
+    return true;
+}
 int main(){
     int tt;
     cin>>tt;
@@ -12,29 +30,41 @@ int main(){
         }
         if(v.size()==2){
             cout<<"Yes"<<"\n";
-        } else {
+        } 
+        else if(check(v)>2){
+            cout<<check(v)<<endl;
+            cout<<"No"<<endl;
+        }
+        else if(check1(v)){
+            cout<<"Yes"<<endl;
+        }
+        else {
             sort(v.begin(),v.end());
-            for(int i=0,j=n-1 ; j>i ; i++,j--){
-                if(i==n-1){
-                    // countd++;
-                    break;
+            int count=0, maxcount=0;
+            for(int i=0 ; i<n ; i++){
+                for(int j=i ; j<n ; j++){
+                    if(j==n-1){
+                        count++;
+                        break;
+                    }
+                    if(v[j]==v[j+1]){
+                        count++;
+                    } else {
+                        count++;
+                        i=j;
+                        break;
+                    }
+                    //cout<<"maxcount="<<maxcount<<"count="<<count<<"i="<<i<<"j="<<j<<endl;
                 }
-                if(v[i]==v[i+1]){                 
-                    v.insert(v.begin()+(i+1),v[j]);
-                    j++;
-                    v.erase(v.begin()+j);
-                    i+=3;
-                }
-                for(int k=0 ; k<n ; k++){
-                    cout<<v[k]<<' ';
-                }
-                cout<<endl;
+                maxcount=max(maxcount,count);
+                count=0;
             }
-            // if(counts==countd||countd==0){
-            //     cout<<"Yes"<<endl;
-            // } else {
-            //     cout<<"No"<<endl;
-            // }
+            //cout<<"maxcount="<<maxcount<<endl;
+            if(maxcount==n/2||v.size()-maxcount==n/2){
+                cout<<"Yes"<<endl;
+            } else {
+                cout<<"No"<<endl;
+            }
         }
     }
     return 0;
