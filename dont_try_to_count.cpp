e8a4f271c3f794dab199ec750b25eab9ba5a1ed6
DaanishShaikh <daanish222@gmail.com>
2024-03-21 15:57:37 +0530

    21-03-2024

--- dont_try_to_count.cpp
+++ dont_try_to_count.cpp
@@ -12,12 +12,45 @@ int main(){
         cin>>s;
         int count=0;
         int p=n*m;
-        bool flag=false;
+        bool flag=true;
         while(x.size()<s.size()){
             x=x+x;
             count++;
         }
         cout<<x<<"\n";
+
+        for(int i=0; i<x.size()-s.size(); i++){
+            for(int j=0 ; j<s.size() ; j++){
+                if(x[i]!=s[j]){
+                    flag=false;
+                    break;
+                } else {
+                    flag=true;
+                }
+            }
+        }
+        if(flag){
+            cout<<count<<"\n";
+        } else {
+            x=x+x;
+            count++;
+            bool flagl=true;
+            for(int i=0; i<x.size()-s.size(); i++){
+            for(int j=0 ; j<s.size() ; j++){
+                if(x[i]!=s[j]){
+                    flagl=false;
+                    break;
+                } else {
+                    flagl=true;
+                }
+            }
+        }
+        if(flag){
+            cout<<count<<"\n";
+        } else {
+            cout<<"-1"<<"\n";
+        }
+        }
     }
     return 0;
 }
