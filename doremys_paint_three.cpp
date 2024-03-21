9fd10fee62d5cf3c33bfd01ed92cc9e58e4ae0d0
DaanishShaikh <daanish222@gmail.com>
2024-03-21 14:03:09 +0530

    21-03-2024

--- doremys_paint_three.cpp
+++ doremys_paint_three.cpp
@@ -1,3 +1,4 @@
+/*accepted*/
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -6,9 +7,20 @@ int main(){
     while(tt--){
         int n;
         cin>>n;
+        map<int, int> maps;
         vector<int>v(n);
         for(int i=0 ; i<n ; i++){
             cin>>v[i];
+            maps[v[i]]++;
+        }
+        if(maps.size()>=3){
+            cout<<"No"<<endl;
+        } else {
+            if(abs(maps.begin()->second-maps.rbegin()->second)<=1){
+                cout<<"Yes"<<endl;
+            } else {
+                cout<<"No"<<endl;
+            }
         }
     }
     return 0;
