4a6bcda5df470179ffb4058ddc97a03543ed6912
DaanishShaikh <daanish222@gmail.com>
2024-03-22 16:17:28 +0530

    22-03-2024

--- dont_try_to_count.cpp
+++ dont_try_to_count.cpp
@@ -4,6 +4,13 @@ int main(){
     int tt;
     cin>>tt;
     while(tt--){
+        int n , m;
+        cin>>n>>m;
+        string x;
+        cin>>x;
+        string s;
+        cin>>s;
+        int count=0;
         
     }
     return 0;
