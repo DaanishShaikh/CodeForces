16abf750468f06b3a0f9c664196a1706f9da79c4
DaanishShaikh <daanish222@gmail.com>
2024-03-23 15:47:14 +0530

    23-03-2024

--- median_of_an_array.cpp
+++ median_of_an_array.cpp
@@ -3,6 +3,12 @@ using namespace std;
 int main(){
     int tt;
     cin>>tt;
-    
+    while(tt--){
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        
+        }
+    }
     return 0;
 }
