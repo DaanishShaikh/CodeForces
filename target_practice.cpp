aa5dfbeb00396259b3097018058d520f00c894b6
DaanishShaikh <daanish222@gmail.com>
2024-03-25 00:47:41 +0530

    25-03-2024

--- target_practice.cpp
+++ target_practice.cpp
@@ -8,7 +8,13 @@ int main(){
         int score=0;
         for(int i=0 ; i<10 ; i++){
             for(int j=0 ; j<10 ; j++){
-                
+                cin>>v[i][j];
+                if(v[i][j]=='X'){
+                    if(i==0 || i==9 || j==0 || j==9){
+                        score+=1;
+                    }
+                    
+                }
             }
         }
         cout<<score<<endl;
