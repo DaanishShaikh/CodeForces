7d11408da36f3cdac235f7c2a95511b8434ba248
DaanishShaikh <daanish222@gmail.com>
2024-03-25 00:47:05 +0530

    25-03-2024

--- target_practice.cpp
+++ target_practice.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 vector<vector<char>>v(10,vector<char>(10,'.'));
 int main(){
-    
+    int tt;
+    cin>>tt;
+    while(tt--){
+        int score=0;
+        
+        cout<<score<<endl;
+    }
     return 0;
 }
