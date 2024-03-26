1b3f911ef0b3ad6ada0b2ef69c4dceaaa1dc3889
DaanishShaikh <daanish222@gmail.com>
2024-03-26 19:57:56 +0530

    26-03-2024

--- sequence_game.cpp
+++ sequence_game.cpp
@@ -1,26 +1,2 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int tt;
-    cin>>tt;
-    while(tt--){
-        int n;
-        cin>>n;
-        vector<int>v;
-        int i=0;
-        for(int i=0 ; i<n ; ++i){
-            int x;
-            cin>>x;
-            if(i&&v.back()>x){
-                v.push_back(1);
-            }
-            v.push_back(x);
-        }
-        cout<<v.size()<<endl;
-        for(int i : v){
-            cout<<i<<' ';
-        }
-        cout<<endl;
-    }
-    return 0;
-}
