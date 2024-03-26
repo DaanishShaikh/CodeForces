a49784779a15e042aac2edc1f8418f9b2e9f7b97
DaanishShaikh <daanish222@gmail.com>
2024-03-26 19:58:57 +0530

    26-03-2024

--- sequence_game.cpp
+++ sequence_game.cpp
@@ -7,7 +7,17 @@ int main(){
         int n;
         cin>>n;
         vector<int>v;
-        
+        int i=0;
+        for(int i=0 ; i<n ; ++i){
+            int x;
+            cin>>x;
+            
+        }
+        cout<<v.size()<<endl;
+        for(int i : v){
+            cout<<i<<' ';
+        }
+        cout<<endl;
     }
     return 0;
 }
