9b5fcbb63b7b38bb460c0edaa38b43037e59212e
DaanishShaikh <daanish222@gmail.com>
2024-03-27 19:38:08 +0530

    27-03-2024

--- flower_city_fence.cpp
+++ flower_city_fence.cpp
@@ -10,8 +10,19 @@ int main(){
         for(int i=0 ; i<n ; i++){
             cin>>v[i];
         }
+        bool flag=1;
         while(true){
-            
+            for(int i=0 ; i<n ; i++){
+                if(v[i]==v.size()-i){
+                    v[i]=0;
+                    for(int j=i ; j<n ; j++){
+                        v[j]--;
+                    }
+                } else {
+                    flag=0;
+                    break;
+                }
+            }
         }
     }
 }
