f5496d4bfd3bbefd164fd7e5a1e91d612880f8d2
DaanishShaikh <daanish222@gmail.com>
2024-03-29 23:11:34 +0530

    29-03-2024

--- upscaling.cpp
+++ upscaling.cpp
@@ -12,8 +12,8 @@ int main(){
         for(int i=0 ; i<2*n ; i++){
             for(int j=0 ; j<2*n ; j++){
                 if(((i/2)+(j/2))%2==0){
-                   
-                }
+                    cout<<'#';
+                } 
             }
             cout<<endl;
         }
