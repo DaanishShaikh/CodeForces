7ebd7728ad137da87f499fa37927310d29b359a1
DaanishShaikh <daanish222@gmail.com>
2024-03-31 00:38:23 +0530

    31-03-2024

--- buttons.cpp
+++ buttons.cpp
@@ -5,10 +5,20 @@ int main(){
     cin>>tt;
     while(tt--){
         int a,b,c;
-        if(a==b){
-            if(c&1==0){
-                cout<<"second"<<endl;
+        cin>>a>>b>>c;
+        if(c%2==0){
+            if(b<a){
+                cout<<"First"<<endl;
+            } else{
+                cout<<"Second"<<"\n";
+            }
+        } else {
+            if(a>=b){
+                cout<<"First"<<"\n";
+            } else{
+                cout<<"Second"<<"\n";
             }
         }
     }
+    return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,36 @@
-                // cout<<"entered if";
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int tt;
+    cin>>tt;
+    while(tt--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a==b){
+            if(c%2==0){
+                cout<<"Second"<<endl;
+            } else{
+                cout<<"First"<<"\n";
+            }
+        } else {
+            if(a>b){
+                a-=b;
+                c-=a;
+            if(c%2==0){
+                cout<<"Second"<<endl;
+            } else{
+                cout<<"First"<<"\n";
+            }
+            } else{
+                b-=a;
+                c-=b;
+            if(c%2==0){
+                cout<<"Second"<<endl;
+            } else{
+                cout<<"First"<<"\n";
+            }
+            }
+        }
+    }
+    return 0;
+}
