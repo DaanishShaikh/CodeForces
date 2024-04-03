1e525b77e0a6f44969e64bc286b73696296fd863
DaanishShaikh <daanish222@gmail.com>
2024-04-03 23:06:03 +0530

    03-04-2024

--- grasshoper_on_a_line.cpp
+++ grasshoper_on_a_line.cpp
@@ -9,7 +9,15 @@ int main(){
         if(a%b!=0){
             cout<<'1'<<endl;
             cout<<a<<endl;
-        }  
+        } else {
+            for(int i=a-1 ; i>0 ; i--){
+                if(i%b!=0){
+                    cout<<'2'<<endl;
+                    cout<<i<<' '<<a-i<<endl;
+                    break;
+                }
+            }
+        } 
     }
     return 0;
 }
