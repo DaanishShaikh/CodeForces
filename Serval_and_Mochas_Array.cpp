2544330fb2bc9aac265073f57fa876a511035847
DaanishShaikh <daanish222@gmail.com>
2024-04-06 00:41:51 +0530

    06-04-2024

--- Serval_and_Mochas_Array.cpp
+++ Serval_and_Mochas_Array.cpp
@@ -11,13 +11,6 @@ int main(){
         for(int i=0 ; i<n ; i++){
             cin>>v[i];
         }
-        
-        bool flag=1;
-        for(int i=0 ; i<n ; i++){
-            if(v[i]%lp!=0){
-                flag=0;
-            }
-        }
         cout<<"lp="<<lp<<endl;
     }
     return 0;
