57d1dfe8eed0455966bfc59840cb2606354b56f6
DaanishShaikh <daanish222@gmail.com>
2024-04-06 00:41:27 +0530

    06-04-2024

--- Serval_and_Mochas_Array.cpp
+++ Serval_and_Mochas_Array.cpp
@@ -7,19 +7,11 @@ int main(){
         int lp=0;
         int n;
         cin >> n;
-        vector<int>c(n);
         vector<int>v(n);
         for(int i=0 ; i<n ; i++){
             cin>>v[i];
         }
-        copy(v.begin(),v.end(),c.begin());
-        sort(c.begin(),c.end());
-        for(int i=0 ; i<n ; i++){
-            if(prime(c[i])){
-                lp=c[i];
-                break;
-            }
-        }
+        
         bool flag=1;
         for(int i=0 ; i<n ; i++){
             if(v[i]%lp!=0){
