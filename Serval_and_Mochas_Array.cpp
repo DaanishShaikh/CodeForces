e7963b4e3ddd097c62187597507f72dba652f777
DaanishShaikh <daanish222@gmail.com>
2024-04-06 12:44:35 +0530

    06-04-2024

--- Serval_and_Mochas_Array.cpp
+++ Serval_and_Mochas_Array.cpp
@@ -1,17 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int tt;
-    cin >> tt;
-    while(tt--){
-        int lp=0;
-        int n;
-        cin >> n;
-        vector<int>v(n);
-        for(int i=0 ; i<n ; i++){
-            cin>>v[i];
+int n,T,a[10005];
+int gcd(int a, int b){
+    int mini=min(a,b);
+    for(int i=2 ; i<=a ; i++){
+        if(a%i==0&&b%i==0){
+            return i;
         }
-        cout<<"lp="<<lp<<endl;
     }
-    return 0;
+    return 1;
+}
+int main(){
+	for (cin>>T; T; T--){
+		cin>>n; int flag=0;
+		for (int i=1; i<=n; i++)cin>>a[i];
+		for (int i=1; i<=n; i++)
+			for (int j=i+1; j<=n; j++) flag |=gcd(a[i],a[j])<=2;
+		cout<<(flag?"Yes":"No")<<endl;
+	}
 }
