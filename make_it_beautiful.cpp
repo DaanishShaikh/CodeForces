49d23b3726e015f8f6e5851d14ceb52ecf57035e
DaanishShaikh <daanish222@gmail.com>
2024-04-06 15:41:13 +0530

    06-04-2024

--- make_it_beautiful.cpp
+++ make_it_beautiful.cpp
@@ -1,40 +1,44 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+bool check(vector<int>v){
+    bool flag=1;
+    for(int i=1 ; i<v.size() ; i++){
+        if(v[i]!=v[0]){
+            flag=0;
+            
+            break;
+        }
+    }
+    return flag;
+}
 int main(){
     int t;
     cin>>t;
     for(int i=0 ; i<t ; i++){
-        int l;
-        cin>>l;
-        int a[l];
-        for(int j=0 ; j<l ; j++){
-            cin>>a[j];
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0 ; i<n ; i++){
+            cin>>v[i];
         }
-        int sum=0;
-         int flag=false;
-        for(int m=0; m<l ; m++){
-            for(int n=0; n<m+1 ; n++){
-                cout<<sum<<' ';
-                sum+=a[n];
-                if(a[m]==sum){
-                    swap(a[n],a[n+1]);
+        if(check(v)){
+            cout<<"NO"<<endl;
+        } else {
+            sort(v.begin(),v.end());
+            vector<int>ans;
+            for(int i=0,j=n-1 ; j>=i ; i++,j--){
+                if(i!=j){
+                ans.push_back(v[i]);
+                ans.push_back(v[j]);
+                } else {
+                    ans.push_back(v[i]);
                 }
-                if(a[m]!=sum){
-                    flag=true;
-                }
+
             }
-            
-            sum=0;
-        }
-    
-        if(flag){
             cout<<"YES"<<endl;
-        for(int o=0 ; o<l ; o++){
-            cout<<a[o];
-        }
-        }
-        else{
-            cout<<"NO";
+            for(int i=0 ; i<ans.size() ; i++){
+                cout<<ans[i]<<' ';
+            } cout<<endl;
         }
     }
         return 0;
