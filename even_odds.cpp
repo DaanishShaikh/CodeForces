01f33e871194eaa715ba2fdfdebd2025fa491e9b
DaanishShaikh <daanish222@gmail.com>
2024-04-11 15:29:33 +0530

    11-04-2024

--- even_odds.cpp
+++ even_odds.cpp
@@ -1,19 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+using i64=long long;
 int main(){
-    int n,k;
+    i64 n,k;
     cin >> n >> k;
     if(n%2){
-        if(k<n/2){
-            cout<<1+((k-1)*2)<<endl;
+        if(k>(n/2)+1){
+            cout<<2+((k-((n/2)+1)-1)*2)<<endl;
         } else {
-            cout<<1+ (2+(((k-n/2)-1)*2))<<endl;            
+            cout<<1+(k-1)*2<<endl;
         }
     } else {
-        if(k%2){
-            cout<<1+((k-1)*2)<<endl;
+        if(k>(n/2)){
+            cout<<2+((k-((n/2))-1)*2)<<endl;
         } else {
-            cout<<n/2 + (2+(k-1)*2)<<endl;            
+            cout<<1+(k-1)*2<<endl;
         }
     }
     return 0;
