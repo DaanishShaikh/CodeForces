44c1889729dcf664cf793fab8e33bcaf964f6fca
DaanishShaikh <daanish222@gmail.com>
2024-04-12 21:18:44 +0530

    12-04-2024

---
+++ expression.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(a==1&&b==1&&c==1){
+        cout<<'3'<<endl;
+    }
+    else if(a==1&&b!=1&&c==1){
+        cout<<b+2<<endl;
+    }
+    else if(a==1&&b==1&&c!=1){
+        cout<<c*2<<endl;
+    }
+    else if(a!=1&&b==1&&c==1){
+        cout<<a*2<<endl;
+    }
+    else if(a==1&&b!=1&&c!=1){
+        cout<<(1+b)*c<<endl;
+    }
+    else if(a!=1&&b==1&&c!=1){
+        cout<<(1+min(a,c))*max(a,c)<<endl;
+    } 
+    else if(a!=1&&b!=1&&c==1){
+        cout<<(1+b)*a<<endl;
+    }
+    else {
+        cout<<a*b*c<<endl;
+    }
+    return 0;
+}
