ce751d599cf88741681ce965dc953cf1c938c5f2
DaanishShaikh <daanish222@gmail.com>
2024-04-15 20:24:54 +0530

    15-04-2024

--- Sort_Me_Please_uwu.cpp
+++ Sort_Me_Please_uwu.cpp
@@ -13,16 +13,23 @@ int main(){
     }
     vector<int>sortindices;
     vector<int>duplicate(n);
+    vector<int>valuesd(n);
+    copy(values.begin(),values.end(),valuesd.begin());
     copy(values.begin(),values.end(),duplicate.begin());
     sort(duplicate.begin(),duplicate.end());
     for(int i=0 ; i<n ; i++){
         for(int j=0 ; j<n ; j++){
-            if(values[i]==duplicate[j]){
+            if(valuesd[i]==duplicate[j]){
                 sortindices.push_back(j);
                 duplicate[j]=-1;
-                values[i]=-1;
+                valuesd[i]=-1;
                 break;
             }
+        }
+    }
+    for(int i=0 ; i<n ; i++){
+        if(sortindices[i]!=i&&values[i]!=values[sortindices[i]]&&colours[i]==colour[sortindices[i]]){
+            count++;
         }
     }
     for(int i=0 ; i<n ; i++){
---
+++ person_recruits.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int count=0;
+    unordered_map<int, int>occ;
+    for(int i=0 ; i<n ; i++){
+        int x;
+        cin >> x;
+        if(x>0){
+            occ[1]=x;
+        }
+        if(x==-1){
+            if(occ[1]>0){
+                occ[1]--;
+            } else {
+                count++;
+            }
+        }
+    }
+    return 0;
+}
