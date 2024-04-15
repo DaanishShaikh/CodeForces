f07fe63bcea55e2d2520857fb118cbb5af90edc5
DaanishShaikh <daanish222@gmail.com>
2024-04-15 22:36:40 +0530

    15-04-2024

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,26 @@
-while(count){
-
-        // }
+#include<bits/stdc++.h>
+using namespace std;
+vector<double>points;
+int main(){
+    double n,l;
+    cin >> n >> l;
+    for(int i=0 ; i<n ; i++){
+        int point;
+        cin >> point;
+        points.push_back(point);
+    }
+    sort(points.begin(),points.end());
+    double diameter=2*max(points[0],l-points[n-1]);
+    for(int i=1 ; i<n ; i++){
+        diameter=max(diameter,points[i]-points[i-1]);
+        //cout<<"diameter"<<i<<"="<<diameter<<endl;
+    }
+    
+    //cout<<"diameter="<<diameter<<endl;
+    double radius=diameter/2;
+    cout<<fixed<<setprecision(10)<<diameter/2.00<<'\n';
+    // for(int i=0 ; i<n ; i++){
+    //     cout << points[i]<<' ';
+    // }
+    return 0;
+}
--- vanya_and_lanterns.cpp
+++ vanya_and_lanterns.cpp
@@ -1,52 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
+vector<double>points;
 int main(){
-    int n;
-    cin>>n;
-    int l;
-    cin>>l;
-    vector<int> v(n);
+    double n,l;
+    cin >> n >> l;
     for(int i=0 ; i<n ; i++){
-        cin>>v[i];
+        int point;
+        cin >> point;
+        points.push_back(point);
     }
+    sort(points.begin(),points.end());
+    double diameter=2*max(points[0],l-points[n-1]);
+    for(int i=1 ; i<n ; i++){
+        diameter=max(diameter,points[i]-points[i-1]);
+        //cout<<"diameter"<<i<<"="<<diameter<<endl;
+    }
+    
+    //cout<<"diameter="<<diameter<<endl;
+    double radius=diameter/2;
+    cout<<fixed<<setprecision(10)<<diameter/2.00<<'\n';
     // for(int i=0 ; i<n ; i++){
-    //     cout<<v[i];
+    //     cout << points[i]<<' ';
     // }
-    sort(v.begin(),v.end());
-    for(int i=0 ; i<n ; i++){
-        cout<<v[i]<<' ';
-    }
-    float max=v[0];
-    for(int i=0 ; i<n ; i++){
-        if(i==n-1){break;}
-        
-            // cout<<"max1="<<max<<' ';
-        }
-        else if(v[i+1]-v[i]>max){
-            max=v[i+1]-v[i];
-            // cout<<"max2="<<max<<' ';
-        }
-        // cout<<"max3="<<max<<endl;
-        if(l-v[n-1]>max){
-            v[n-1]=max;
-        }
-        if(max==317950*2){
-            int test=i;
-            int num=v[i];
-            break;
-    cout<<"test="<<test<<"num="<<num;
-        }
-    }
-    if(v[0]!=0){
-            if(v[0]>max){
-                max=v[0]+1;
-            }
-    if(max==v[0]){
-        cout<<max;
-    }
-    else{
-        cout<<max/2;
-    }
-
     return 0;
 }
