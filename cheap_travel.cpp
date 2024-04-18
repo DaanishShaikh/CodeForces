483c367e3772addf318d5dcd70d546fc7ba2a8e9
DaanishShaikh <daanish222@gmail.com>
2024-04-18 08:08:39 +0530

    18-04-2024

--- cheap_travel.cpp
+++ cheap_travel.cpp
@@ -4,9 +4,8 @@ int main(){
     int n,m,a,b;
     cin >> n >> m >> a >> b;
     int cost=0;
-    if(a*m>b && m<=n){
-        cost+=(n/m)*b;
-        cost+=(n%m)*a;
+    if(a*m>b){
+        cost+=((n/m)*b)+min((n%m)*a,b);
         cout<<cost<<endl;
     } else {
         cout<<n*a<<endl;
