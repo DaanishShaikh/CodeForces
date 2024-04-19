0c5050724690fb43469d6741e47ee0735bd41d5f
DaanishShaikh <daanish222@gmail.com>
2024-04-19 18:38:26 +0530

    19-04-2024

---
+++ plus_or_minus.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int tt;
+    cin >> tt;
+    while(tt--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        if(a+b==c){
+            cout<<'+'<<endl;
+        } 
+        else if(a-b==c){
+            cout<<'-'<<endl;
+        }      
+    }
+    return 0;
+}
