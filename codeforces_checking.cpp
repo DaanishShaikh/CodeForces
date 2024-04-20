147f8a9418211ad706430dea4c673f568c5fb39b
DaanishShaikh <daanish222@gmail.com>
2024-04-20 08:54:33 +0530

    20-04-2024

---
+++ codeforces_checking.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int tt;
+    cin >> tt;
+    while(tt--){
+    char c;
+    cin >> c;
+    if(c=='c' || c=='o' || c=='d' || c=='e' || c=='f' || c=='r' || c=='s'){
+        cout<<"YES"<<endl;
+    } else {
+        cout<<"NO"<<endl;
+    }
+    }
+    return 0;
+}
