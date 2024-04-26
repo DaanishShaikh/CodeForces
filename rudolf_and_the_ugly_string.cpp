18c2bf795b0ba2bf7abaacd607f967e6a6fd990f
DaanishShaikh <daanish222@gmail.com>
2024-04-26 08:32:25 +0530

    26-04-2024

--- rudolf_and_the_ugly_string.cpp
+++ rudolf_and_the_ugly_string.cpp
@@ -10,7 +10,11 @@ int main(){
     cin >> s;
     int count=0;
     for(int i=2 ; i<s.size() ; i++){
-        if((s[i-2]=='m' && s[i-1]=='a' && s[i]=='p') || (s[i-2]=='p' && s[i-1]=='i' && s[i]=='e')){
+        if(s[i-2]=='m' && s[i-1]=='a' && s[i]=='p' && s[i+1]=='i' && s[i+2]=='e'){
+            count++;
+            i+=2;
+        }
+        else if((s[i-2]=='m' && s[i-1]=='a' && s[i]=='p') || (s[i-2]=='p' && s[i-1]=='i' && s[i]=='e')){
             count++;
         }
     }
