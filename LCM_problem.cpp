3562d130231f8c75b8401906329da6c581c586cc
DaanishShaikh <daanish222@gmail.com>
2024-05-03 12:11:45 +0530

    01-05-2024

---
+++ LCM_problem.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using i64=long long;
+
+void solve(){
+    int n,m;
+    cin >> n >> m;
+    for(int x=n ; x<=m ; x++){
+        for(int y=n ; y<=m ; y++){
+            
+        }
+    }
+
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt;
+    cin >> tt;
+
+    while(tt--){
+        solve();
+    }
+
+    return 0;
+}
