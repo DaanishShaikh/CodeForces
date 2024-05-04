816e1f2e81d01123c0387e1eb1ab32daded654eb
DaanishShaikh <daanish222@gmail.com>
2024-05-04 15:49:34 +0530

    04-05-2024

---
+++ EhAb_AnD_gCd.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using i64=long long;
+
+void solve(){
+    
+    int n;
+    cin >> n;
+    cout<<1<<' '<<n-1<<endl;
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt;
+    cin >> tt;
+
+    while(tt--){
+        solve();
+    }
+
+    return 0;
+}
