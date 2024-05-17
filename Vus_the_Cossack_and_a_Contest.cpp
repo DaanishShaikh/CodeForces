aacf299d9abf265801b5314c850137d844bc2281
DaanishShaikh <daanish222@gmail.com>
2024-05-17 08:45:48 +0530

    15-05-2024

---
+++ Vus_the_Cossack_and_a_Contest.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using i64=long long;
+
+void solve(){
+    
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(b<a||c<a){
+        cout<<"No"<<endl;
+    } else {
+        cout<<"Yes"<<endl;
+    }
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+   
+        solve();
+
+    return 0;
+}
