cfb3878944bcaf9ca0c23d74bbd02c864307f684
DaanishShaikh <daanish222@gmail.com>
2024-05-19 15:36:12 +0530

    19-05-2024

---
+++ FashionabLee.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using i64=long long;
+
+void solve(){
+    
+    int n;
+    cin >> n;
+    if(n%4==0){
+        cout<<"YES"<<endl;
+    } else {
+        cout<<"NO"<<endl;
+    }
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt;
+    cin >> tt;
+
+    while(tt--){
+        solve();
+    }
+
+    return 0;
+}
