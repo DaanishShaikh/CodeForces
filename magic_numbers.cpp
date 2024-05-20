74f146ceeefbe3646b3e0ed3972d70f4b957e054
DaanishShaikh <daanish222@gmail.com>
2024-05-20 22:26:51 +0530

    20-05-2024

---
+++ magic_numbers.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using i64=long long;
+
+void solve(){
+    
+    string s;
+    cin >> s;
+    int count=0;
+    for(int i=0 ; i<s.size() ; i++){
+        if(s[i]=='4'){
+            count++;
+        } else {
+            count=0;
+            if(count>=3){
+                count=0;
+                cout<<"NO"<<endl;
+                return;
+            }
+        }
+        if(s[i]!='4'&&s[i]!='1'){
+            cout<<"NO"<<endl;
+            return;
+        }
+        if(i==s.size()-1&&count>=3){
+            cout<<"NO"<<endl;
+            return;
+        }
+    }
+    cout<<"YES"<<endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+        solve();
+
+    return 0;
+}
