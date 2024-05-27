249a2cd78b7ea45eaf56e0a7c416fd0fdd770233
DaanishShaikh <daanish222@gmail.com>
2024-05-27 08:24:55 +0530

    27-05-2024

--- binary_colouring.cpp
+++ binary_colouring.cpp
@@ -7,151 +7,16 @@ void solve(){
     
     i64 n;
     cin >> n;
-    if(n==1){
-        cout<<1<<endl<<1<<endl;
-        return;
+    vector<int>v;
+    while(n){
+        v.push_back(n%2);
+        n/=2;
     }
-    bool odd=0;
-    // if(n%2){
-    //     odd=1;
-    //     n--;
-    // }
-    int x=-1,y=-1;
-    if(n%2==0){
-    for(int i=0 ; i<=32 ; i++){
-        for(int j=0 ; j<=32 ; j++){
-            if(pow(2,i)-pow(2,j)==n){
-                x=i;
-                y=j;
-                cout<<"x="<<x<<' '<<"y="<<y<<endl;
-                break;
-            }
-        }
+    vector<int>ans;
+    for(int i=v.size()-1 ;i>=0 ; i--){
+        ans.push_back(v[i]);
     }
     
-    
-        vector<int>ans(x+1);
-        ans[y]=-1;
-        ans[x]=1;
-        for(int i=0 ; i<ans.size() ; i++){
-            if(i!=y&&i!=x){
-                ans[i]=0;
-            }
-        }
-        cout<<ans.size()<<endl;
-        for(int i=0 ; i<ans.size() ; i++){
-            cout<<ans[i]<<' ';
-        }
-        cout<<endl;
-    } else {
-        n++;
-        for(int i=0 ; i<32 ; i++){
-        for(int j=0 ; j<32 ; j++){
-            if(pow(2,i)-pow(2,j)==n){
-                x=i;
-                y=j;
-                cout<<"x="<<x<<' '<<"y="<<y<<endl;
-                break;
-            }
-        }
-    }
-        vector<int>ans(x+1);
-        ans[y]=-1;
-        ans[x]=1;
-        for(int i=0 ; i<ans.size() ; i++){
-            if(i!=y&&i!=x){
-                ans[i]=0;
-            }
-        }
-        ans[0]=-1;
-        cout<<ans.size()<<endl;
-        for(int i=0 ; i<ans.size() ; i++){
-            cout<<ans[i]<<' ';
-        }
-        cout<<endl;
-
-    }
-    // else {
-    //     for(int i=0 ; i<y-1 ; i++){
-    //         ans.push_back(0);
-    //     }
-    //     ans.push_back(-1);
-    //     for(int i=0 ; i<x-y ; i++){
-    //         ans.push_back(0);
-    //     }
-    //     ans.push_back(1);
-    //     cout<<ans.size()<<endl;
-    //     for(int i=0 ; i<ans.size() ; i++){
-    //         cout<<ans[i]<<' ';
-    //     }
-    //     cout<<endl;
-    // }
-    if(x==-1||y==-1){
-        if(n==1){
-        cout<<1<<endl<<1<<endl;
-        return;
-    }
-    bool odd=0;
-    // if(n%2){
-    //     odd=1;
-    //     n--;
-    // }
-    int x=-1,y=-1;
-    if(n%2==0){
-    for(int i=0 ; i<=32 ; i++){
-        for(int j=0 ; j<=32 ; j++){
-            if(pow(2,i)+pow(2,j)==n){
-                x=i;
-                y=j;
-                cout<<"x="<<x<<' '<<"y="<<y<<endl;
-                break;
-            }
-        }
-    }
-    
-    
-        vector<int>ans(x+1);
-        ans[y]=1;
-        ans[x]=1;
-        for(int i=0 ; i<ans.size() ; i++){
-            if(i!=y&&i!=x){
-                ans[i]=0;
-            }
-        }
-        cout<<ans.size()<<endl;
-        for(int i=0 ; i<ans.size() ; i++){
-            cout<<ans[i]<<' ';
-        }
-        cout<<endl;
-    } else {
-        n++;
-        for(int i=0 ; i<32 ; i++){
-        for(int j=0 ; j<32 ; j++){
-            if(pow(2,i)+pow(2,j)==n){
-                x=i;
-                y=j;
-                cout<<"x="<<x<<' '<<"y="<<y<<endl;
-                break;
-            }
-        }
-    }
-        vector<int>ans(x+1);
-        ans[y]=1;
-        ans[x]=1;
-        for(int i=0 ; i<ans.size() ; i++){
-            if(i!=y&&i!=x){
-                ans[i]=0;
-            }
-        }
-        ans[0]=-1;
-        cout<<ans.size()<<endl;
-        for(int i=0 ; i<ans.size() ; i++){
-            cout<<ans[i]<<' ';
-        }
-        cout<<endl;
-
-    }
-    }
 }
 
 int main(){
