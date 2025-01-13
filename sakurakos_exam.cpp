0ccebed987432cc5d90470257c99a1cf773cbbbf
daanish <daanish222@gmail.com>
2025-01-13 23:50:12 +0530

    13-01-2025

--- sakurakos_exam.cpp
+++ sakurakos_exam.cpp
@@ -6,46 +6,27 @@ using i64 = long long;
 void solve() {
     int n,m;
     cin >> n >> m;
-    if(n==0&&m==0){
-    cout<<"YES"<<'\n';
-    return;
-    }
-    if(n%2){
-        cout<<"NO"<<'\n';
-        return;
-    }
-        if(n==0&&m){
-            if(m%2==0){
-            cout<<"YES"<<'\n';
-            return;
-            } else {
-            cout<<"NO"<<'\n';
-            return;
-            }
-        }    
-        if(n&&m==0){
-            if(n%2==0){
-            cout<<"YES"<<'\n';
-            return;
-            } else {
-                cout<<"NO"<<'\n';
-                return;
-            }
+    if(n%2)
+        {
+            cout<<"NO"<<endl;
         }
-        // if(n&&m){
-        //     if(m%2==0){
-        //     cout<<"YES"<<'\n';
-        //     return;
-        //     } else {
-        //         cout<<"NO"<<'\n';
-        //         return;
-        //     }
-        // }
-        if((2*m)-n){
-            cout<<"NO"<<"blah blah"<<'\n';
-            return;
-        } else {
-            cout<<"YES"<<'\n';
+        else
+        {
+            if(m%2==0)
+            {
+                cout<<"YES"<<endl;
+            }
+            else
+            {
+                if(n==0)
+                {
+                    cout<<"NO"<<endl;
+                }
+                else
+                {
+                    cout<<"YES"<<endl;
+                }
+            }
         }
 }
 int main() {
