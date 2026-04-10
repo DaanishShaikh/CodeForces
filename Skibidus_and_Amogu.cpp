8c780ccf91fc9d342c3c81f5ac82c1d78dbf3749
daanish <daanish222@gmail.com>
2026-04-10 18:32:20 +0530

    10-04-2026

---
+++ Skibidus_and_Amogu.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+ 
+// using u128 = unsigned__int128;
+// using i128 = __int128;
+ 
+void solve() {
+   string s;
+   cin >> s;
+   string ans="";
+   for(int i=0 ; i<s.size()-2 ; i++) {
+    ans+=s[i];
+   }
+   ans+='i';
+   cout<<ans<<"\n";
+}
+ 
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int t;
+    std::cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
