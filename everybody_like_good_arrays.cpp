e8fd37bdd8c45baed9842500b3ff7a54909b3208
daanish <daanish222@gmail.com>
2026-04-13 17:58:51 +0530

    13-04-2026

--- everybody_like_good_arrays.cpp
+++ everybody_like_good_arrays.cpp
@@ -1,67 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-using i64=long long;
-bool check(vector<int>v){
-    bool flag=1;
-    for(int i=0 ; i<v.size() ; i++){
-        if(v[i]!=v[0]){
-            flag=0;
-        }
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+ 
+// using u128 = unsigned__int128;
+// using i128 = __int128;
+ 
+void solve() {
+    
+   int n;
+   cin >> n;
+   vector<int> a(n);
+   for (int i = 0; i < n; i++) {
+       cin >> a[i];
+   }
+   int ans=0, count=0;
+   bool flag = false;
+   for(int i = 0; i < n-1; i++) {
+    if(a[i+1]%2==a[i]%2) {
+        ans++;
     }
-    return flag;
+   }
+   cout<<ans<<'\n';
 }
-int main(){
-    int tt;
-    cin >> tt;
-    while(tt--){
-        int count=0,totalcount=0;
-        i64 n;
-        cin >> n;
-        vector<int>v(n);
-        for(int i=0 ; i<n ; i++){
-            cin>>v[i];
-        }
-        
-        vector<int>parity;
-        for(int i=0 ; i<n ; i++){
-            if(v[i]%2==1){
-                parity.push_back(1);
-            } else {
-                parity.push_back(-1);
-            }
-        }
-        if(check(parity)){
-            cout<<parity.size()-1<<endl;
-        } else {
-        // for(int i = 0 ; i<parity.size() ; i++){
-        //     cout<<parity[i]<<' ';
-        // }
-        // cout<<endl;
-        for(int i =0,j=0 ; i<parity.size()-j ; i++){
-            if(i==parity.size()-1){
-                break;
-            }
-            if(parity[i]==1&&parity[i+1]==1){
-                //cout<<"count="<<count<<"parity["<<i<<"]="<<parity[i]<<"parity["<<i+1<<"]="<<parity[i+1]<<endl;
-                count++;
-                parity.erase(parity.begin()+i);
-                j++;
-                i--;
-            }
-            if(parity[i]==-1&&parity[i+1]==-1){
-                //cout<<"count="<<count<<"parity["<<i<<"]="<<parity[i]<<"parity["<<i+1<<"]="<<parity[i+1]<<endl;
-                count++;
-                parity.erase(parity.begin()+i);
-                j++;
-                i--;
-            }
-        }
-        // for(int i = 0 ; i<parity.size() ; i++){
-        //     cout<<parity[i]<<' ';
-        // }
-        // cout<<endl;
-        cout<<count<<endl;
-        }
+ 
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int t;
+    std::cin >> t;
+    
+    while (t--) {
+        solve();
     }
+    
     return 0;
 }
