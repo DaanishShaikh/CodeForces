0f45c019697fc7efa931c63c253233c3412dd2a3
daanish <daanish222@gmail.com>
2026-04-27 20:33:37 +0530

    27-04-2026

---
+++ homework.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+ 
+// using u128 = unsigned__int128;
+// using i128 = __int128;
+ 
+void solve() {
+   int n;
+   cin >> n;
+   string a;
+    cin >> a;
+    int m;
+    cin >> m;
+    string b;
+    cin >> b;
+    string c;
+    cin >> c;
+    for(int i = 0; i < c.size(); i++) {
+        if(c[i] == 'V') {
+            a=b[i]+a;
+        } else {
+            a=a+b[i];
+        }
+    }
+    cout<<a<<'\n';
+}
+ 
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int t;
+    std::cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
